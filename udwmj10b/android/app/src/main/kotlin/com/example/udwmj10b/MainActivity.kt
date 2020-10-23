9f5a49a61020fa043d42538909ce7d36a961a5e7
Ayer Ribeiro <ayer.ribeiro@letras.mus.br>
2020-10-23 20:07:08 -0300

    Prática 10-b

---
+++ udwmj10b/android/app/src/main/kotlin/com/example/udwmj10b/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.udwmj10b
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ udwmj10b/lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatefulWidget {
+  HomeState createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+  String resp = "";
+
+  String getClassificacaoPorIMC(double imc) {
+    if (imc < 16) {
+      return "Magreza grave";
+    } else if (imc >= 16 && imc < 17) {
+      return "Magreza moderada";
+    } else if (imc >= 17 && imc < 18.5) {
+      return "Magreza leve";
+    } else if (imc >= 18.5 && imc < 25) {
+      return "Saudável";
+    } else if (imc >= 25 && imc < 30) {
+      return "Sobrepeso";
+    } else if (imc >= 30 && imc < 35) {
+      return "Obesidade Grau I";
+    } else if (imc >= 35 && imc < 40) {
+      return "Obesidade Grau II (severa)";
+    } else {
+      return "Obesidade Grau III (mórbida)";
+    }
+  }
+
+  calcular() {
+    double peso = double.tryParse(this.pesoController.text);
+    double altura = double.tryParse(this.alturaController.text);
+    double imc = peso / (altura * altura);
+    this.resp =
+        'IMC = ${imc.toStringAsFixed(2)}, ${getClassificacaoPorIMC(imc)}';
+    return this.resp;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Página Inicial'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            child: TextField(
+              controller: pesoController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  onPressed: () => pesoController.clear(),
+                  icon: Icon(Icons.clear),
+                ),
+                border: OutlineInputBorder(),
+                labelText: 'informe o peso',
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: TextField(
+              controller: alturaController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  onPressed: () => alturaController.clear(),
+                  icon: Icon(Icons.clear),
+                ),
+                border: OutlineInputBorder(),
+                labelText: 'informe a altura',
+              ),
+            ),
+          ),
+          RaisedButton(
+            color: Colors.blue,
+            textColor: Colors.white,
+            onPressed: () {
+              print(calcular());
+              setState(calcular);
+            },
+            child: Text(
+              'Calcular',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Text(
+            this.resp,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.green,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
